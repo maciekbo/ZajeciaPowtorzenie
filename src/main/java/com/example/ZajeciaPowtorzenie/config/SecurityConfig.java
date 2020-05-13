2fcdd848f9f70c7777d2e2f3a34997630b61bc1d
Maciek <maciejboron22@gmail.com>
2020-05-13 16:15:34 +0200

    Add loging in for squad and admin loging for student.

--- src/main/java/com/example/ZajeciaPowtorzenie/config/SecurityConfig.java
+++ src/main/java/com/example/ZajeciaPowtorzenie/config/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.example.ZajeciaPowtorzenie.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user1")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin1")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/student").hasRole("ADMIN")
+                .antMatchers("/squad").hasAnyRole()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().permitAll();
+    }
 }
